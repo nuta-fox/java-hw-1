24142d11f5dd4ab62a28afc3ccc559152df875f3
nuta-fox <jdanova.a.s@gmail.com>
2021-11-25 14:05:52 +0600

    first

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2_000_000_000;
+        int count = 500_000_000;
+        int total = price * count;
+        System.out.println(total);
+    }
+}
+
